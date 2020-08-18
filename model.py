67c2107d2804b4fdc754636ccd45ffd53468347e
tyg04308 <tyg04308@naver.com>
2020-08-18 17:47:40 +0900

    add 5th layer

--- model.py
+++ model.py
@@ -2,18 +2,18 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-cfg = {'resnet18': [2, 2, 2, 2], 'resnet34': [3, 4, 6, 3]}
+cfg = {'resnet18': [2, 2, 2, 2], 'resnet34': [3, 4, 6, 3], 'resnet50': [3, 4, 6, 3], 'resnet101': [3, 4, 23, 3], 'resnet152': [3, 8, 36, 3]}
 
-class Basicblock(nn.Module):
-    def __init__(self, in_channels, out_channels, stride=1, padding=1, kernel_size=3):
-        super(Basicblock, self).__init__()
+class BasicBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, stride=1, padding=1):
+        super(BasicBlock, self).__init__()
 
-        self.conv1 = nn.Conv2d(in_channels=in_channels, out_channels=out_channels, stride=stride, padding=padding, kernel_size=kernel_size, bias=False)
+        self.conv1 = nn.Conv2d(in_channels=in_channels, out_channels=out_channels, stride=stride, padding=padding, kernel_size=3, bias=False)
 
         self.bn = nn.BatchNorm2d(out_channels)
 
         self.conv2 = nn.Conv2d(in_channels=out_channels, out_channels=out_channels,
-                stride=1, padding=1, kernel_size=kernel_size, bias=False)
+                stride=1, padding=1, kernel_size=3, bias=False)
 
         self.bn2 = nn.BatchNorm2d(out_channels)
 
@@ -48,18 +48,74 @@ class Basicblock(nn.Module):
 
         return x
 
+class BottleneckBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, stride=1, padding=1):
+        super(BottleneckBlock, self).__init__()
+
+        self.conv1 = nn.Conv2d(in_channels=in_channels, out_channels=out_channels, kernel_size=1, bias=False)
+
+        self.bn = nn.BatchNorm2d(out_channels)
+
+        self.conv2 = nn.Conv2d(in_channels=out_channels, out_channels=out_channels,
+                stride=stride, padding=1, kernel_size=3, bias=False)
+
+        self.bn2 = nn.BatchNorm2d(out_channels)
+
+        self.conv3 = nn.Conv2d(in_channels=out_channels, out_channels=out_channels*4, kernel_size=1, bias=False)
+
+        self.bn3 = nn.BatchNorm2d(out_channels*4)
+
+        self.relu = nn.ReLU(inplace=True)
+
+        self.increase = None
+
+        nn.init.constant_(self.bn2.weight, 0)
+        
+
+        if stride != 1 or in_channels != out_channels*4:
+            self.increase = nn.Sequential(
+                    nn.Conv2d(kernel_size=1, in_channels=in_channels, out_channels=out_channels*4,
+                        stride=stride, bias=False),
+                    nn.BatchNorm2d(out_channels*4))
+
+    def forward(self, x):
+        identity = x
+
+        x = self.conv1(x)
+        x = self.bn(x)
+        x = self.relu(x)
+
+        x = self.conv2(x)
+        x = self.bn2(x)
+        x = self.relu(x)
+
+        x = self.conv3(x)
+        x = self.bn3(x)
+
+        if self.increase is not None:
+            identity = self.increase(identity)
+
+        x += identity
+        x = self.relu(x)
+
+        return x
 
 class ResNet(nn.Module):
-    def __init__(self, model='resnet18'):
+    def __init__(self, model='resnet50'):
         super(ResNet, self).__init__()
 
         self.model = model
         self.inplanes = 64
-        block = Basicblock
+        self.expansion = 1
+        block = BasicBlock
+
+        if (self.model == 'resnet50' or self.model == 'resnet101' or self.model == 'resnet152'):
+            block = BottleneckBlock
+            self.expansion = 4
         
         self.layer1 = nn.Sequential(
                 nn.Conv2d(in_channels=3, out_channels=self.inplanes, kernel_size=3,
-                        padding=1, stride=1),
+                        padding=1, stride=1, bias=False),
                 nn.BatchNorm2d(self.inplanes))
 
         self.layer2 = self.get_layer(block, channels=64, stride=1, n=cfg[self.model][0])
@@ -68,10 +124,7 @@ class ResNet(nn.Module):
         self.layer5 = self.get_layer(block, channels=512, stride=2, n=cfg[self.model][3])
 
         self.fc = nn.Sequential(
-                nn.Linear(1024, 512),
-                nn.ReLU(inplace=True),
-                nn.Dropout(p=0.5),
-                nn.Linear(512, 10))
+                nn.Linear(512*self.expansion, 10))
 
         for m in self.modules():
             if isinstance(m, nn.Conv2d):
@@ -85,7 +138,7 @@ class ResNet(nn.Module):
         layers = [block(in_channels=self.inplanes, out_channels=channels,
             stride=stride)]
 
-        self.inplanes = channels
+        self.inplanes = channels * self.expansion
 
         for i in range(n-1):
             layers.append(block(in_channels=self.inplanes, out_channels=channels))
@@ -100,6 +153,7 @@ class ResNet(nn.Module):
         x = self.layer2(x)
         x = self.layer3(x)
         x = self.layer4(x)
+        x = self.layer5(x)
 
         x = F.avg_pool2d(x, 4)
         x = x.view(x.size(0), -1)
@@ -111,7 +165,7 @@ class ResNet(nn.Module):
 if __name__ == '__main__':
     dummy_data = torch.rand(4, 3, 32, 32)
 
-    resnet = ResNet()
+    resnet = ResNet('resnet101')
 
     x = resnet(dummy_data)
 
--- train.py
+++ train.py
@@ -4,6 +4,7 @@ import time
 import sys
 import torch.nn as nn
 import argparse
+import os
 
 from torchvision.datasets import CIFAR10
 from torch.utils.data import DataLoader
@@ -12,8 +13,8 @@ from model import ResNet
 batch_size=256
 momentum=0.9
 weight_decay = 0.005
-learning_rate = 0.01
-epochs = 135
+learning_rate = 0.1
+epochs = 100
 is_cuda = torch.cuda.is_available()
 device = torch.device('cuda' if is_cuda else 'cpu')
 
@@ -27,6 +28,11 @@ def get_parser():
     parser = argparse.ArgumentParser(description='VGG16')
     parser.add_argument('--gpu', type=int, default=-1,
             help='gpu number')
+
+    parser.add_argument('--model', type=str, default='resnet18',
+            help='model type')
+    
+    parser.add_argument('--result', type=str, default='weight')
 
     args = parser.parse_args()
 
@@ -45,15 +51,7 @@ def main():
 
     mean, std = get_mean_std(cifar10_dataset)
 
-    transform_v1 = transforms.Compose([
-        transforms.RandomCrop(32),
-        transforms.RandomHorizontalFlip(),
-        transforms.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2),
-        transforms.ToTensor(),
-        transforms.Normalize(mean=mean, std=std)])
-
-    transform_v2 = transforms.Compose([
-        transforms.Resize(64),
+    train_transform = transforms.Compose([
         transforms.RandomCrop(32),
         transforms.ToTensor(),
         transforms.Normalize(mean=mean, std=std)])
@@ -64,23 +62,17 @@ def main():
 
     print ("\nLoading Cifar 10 Dataset...")
 
-    train1 = CIFAR10(root='./dataset', train=True, download=True,
-        transform=transform)
+    train_dataset = CIFAR10(root='./dataset', train=True, download=True,
+        transform=train_transform)
 
-    train2 = CIFAR10(root='./dataset', train=True, download=False,
-        transform=transform_v1)
-
-    train3 = CIFAR10(root='./dataset', train=True, download=False,
-        transform=transform_v2)
-
-    train_loader = DataLoader(train1+train2+train3, batch_size=batch_size,
-            shuffle=True, num_workers=4)
+    train_loader = DataLoader(train_dataset, batch_size=batch_size,
+            shuffle=True, num_workers=8)
 
     val_dataset = CIFAR10(root='./dataset', train=False,
             download = True, transform=transform)
 
     val_loader = DataLoader(val_dataset, batch_size=batch_size,
-            shuffle=False, num_workers=4)
+            shuffle=False, num_workers=8)
 
     classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog',
             'horse', 'ship', 'truck')
@@ -89,14 +81,18 @@ def main():
 
     print ("\n========================================\n")
 
-    resnet = ResNet()
+    if not os.path.isdir(parser.result):
+        os.mkdir(parser.result)
+
+    resnet = ResNet(parser.model)
+    print (f"Loaded {parser.model}\n\n")
 
     global learning_rate
 
     optimizer = torch.optim.SGD(resnet.parameters(), lr=learning_rate, momentum=momentum,
             weight_decay=weight_decay)
     criterion = nn.CrossEntropyLoss()
-    scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer, milestones=[65, 95, 115])
+    scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer, milestones=[50, 75, 90])
 
     best_acc = 0.0
     best_loss = 9.0
@@ -125,12 +121,11 @@ def main():
             best_acc = acc
 
         if is_best:
-            torch.save(resnet.state_dict(), "./weight/best_weight.pth")
+            torch.save(resnet.state_dict(), parser.result + "/best_"+ parser.model +".pth")
             print (f"\nSave best model at acc: {acc:.4f},  loss: {loss:.4f}!")
 
         print ("\n========================================\n")
 
-        torch.save(resnet.state_dict(), "./weight/lastest_weight.pth")
 
 def train(train_loader, model, criterion, optimizer, scheduler, epoch):
     model.train()
